7ac3e8f002543b5db1bc394b9e4a84bc07d9ca34
BT_1N3_14@bktech.local <BT_1N3_14@bktech.local>
2018-09-18 09:17:08 -0400

    Initial commit

---
+++ src/soconnor/soconnorLib.java
@@ -0,0 +1,10 @@
+package soconnor;
+
+public class soconnorLib
+{
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+
+}
---
+++ src/soconnor/testfile.java
@@ -0,0 +1,11 @@
+package soconnor;
+
+import static soconnor.soconnorLib.println;
+
+public class testfile
+{
+    public static void main(String[] args)
+    {
+        println("I love a cup of java in the morning.");
+    }
+}
